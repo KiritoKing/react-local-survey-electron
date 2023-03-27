4eb67ea8a318778b98ac1302afdae4131bbc88a8
KiritoKing <kiritoclzh@gmail.com>
2023-03-27 23:55:00 +0800

    修改survey页获取数据逻辑，改用context避免重复读取构造

--- src/renderer/Pages/Survey/index.tsx
+++ src/renderer/Pages/Survey/index.tsx
@@ -1,20 +1,17 @@
 import { ISurveyCache } from 'main/typing';
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Survey } from 'survey-react-ui';
 import { Model } from 'survey-core';
 import 'survey-core/defaultV2.min.css';
 import 'survey-core/survey.i18n';
+import { SurveyListContext } from 'renderer/App';
 
 const SurveyPage = () => {
   const { surveyId } = useParams();
   const nav = useNavigate();
-  const surveys = JSON.parse(
-    window.localStorage.getItem('surveyCache') as string // 该页面理论上不可能出现没有缓存的情况
-  );
-  const survey = (surveys as ISurveyCache[]).find(
-    (item) => item.id === surveyId
-  );
+  const surveys = (useContext(SurveyListContext) as any).data as ISurveyCache[];
+  const survey = surveys.find((item) => item.id === surveyId);
   const model = new Model(survey!.data);
   const handleComplete = useCallback((sender: any) => {
     console.log(sender.data);
