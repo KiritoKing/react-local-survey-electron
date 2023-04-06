0582f16e88fea92cf8bf309cf59a3fcf41f82a1c
KiritoKing <kiritoclzh@gmail.com>
2023-04-06 19:49:27 +0800

    clear code in float panel plan

--- src/renderer/Components/SurveyEditor/index.tsx
+++ src/renderer/Components/SurveyEditor/index.tsx
@@ -15,94 +15,24 @@ interface IHeight {
 
 // TODO: 右侧按钮随左侧内容全量刷新，考虑使用内部渲染方案
 const SurveyEditor: React.FC<IProps> = ({ data: model }) => {
-  const [segments, setSegments] = useState<ISegment[]>([]);
-  let heightTemp: IHeight[] = [];
-  const addHeightTemp = (id: string, height: number) => {
-    if (heightTemp.find((p) => p.elementId === id)) return;
-    console.log(`Element ${id} height updated to ${height}`);
-    heightTemp = [...heightTemp, { height, elementId: id }];
-  };
-
-  const refreshSegements = useCallback(
-    (data: Model) => {
-      const newSegments: ISegment[] = [];
-
-      const addSegement = (
-        element: IElement,
-        prev: ISegment[],
-        target: ISegment[]
-      ) => {
-        // 刷新时寻找之前的列表中是否已有该元素
-        const existed = prev.find(
-          (p) => (p.data as any).id === (element as any).id
-        );
-
-        if (existed !== undefined) target.push(existed); // 如果已存在则直接添加
-        else
-          target.push({
-            height:
-              heightTemp.find((h) => h.elementId === (element as any).id)
-                ?.height ?? 0,
-            data: element,
-          }); // 首先插入当前元素（panel本身也要插入作占位符）
-
-        if (element.isPanel) {
-          target[target.length - 1].height = 87; // Panel占位符固定高度
-          const panel = element as PanelModel;
-          panel.elements.forEach((question) => {
-            addSegement(question, prev, target);
-          });
-        }
-      };
-
-      data.pages[data.currentPageNo].elements.forEach((element) =>
-        addSegement(element, segments, newSegments)
-      );
-
-      return newSegments;
-    },
-    [heightTemp, segments]
-  );
-
-  useEffect(() => {
-    console.log('Update segments');
-    segments.forEach((s) => {
-      console.log(`${s.data.name}=${s.height}`);
-    });
-    setSegments(refreshSegements(model));
-  }, [heightTemp]);
-
-  model.locale = 'zh-cn';
-  model.currentPage;
-
-  model.onAfterRenderQuestion.add(
-    useCallback((sender, options) => {
-      console.log(`Element ${options.question.id} rendered`);
-      const { id } = options.question;
-      const { height } = options.htmlElement.getBoundingClientRect();
-      addHeightTemp(id, height);
-    }, [])
-  );
-
-  model.onAfterRenderPanel.add(
-    useCallback((sender, options) => {
-      const { elements } = options.panel;
-    }, [])
-  );
-
+  model.onAfterRenderQuestion.add((survey, options) => {
+    console.log(`onAfterRenderQuestion: ${options.question.name}`);
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.className = 'dialogBox-btn';
+    btn.innerHTML = 'More Info';
+    const header = options.htmlElement.querySelector('h5');
+    header?.appendChild(btn);
+  });
   return (
     <Box
       sx={{
         display: 'flex',
-        flexDirection: 'row',
         mt: 2,
         position: 'relative',
       }}
     >
-      <Box sx={{ width: '70%' }}>
-        <Survey model={model} mode="display" />
-      </Box>
-      <EditPanel segments={segments} model={model} />
+      <Survey model={model} mode="display" />
     </Box>
   );
 };
